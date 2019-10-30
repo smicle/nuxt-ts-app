bf173610dec3c5989af3e4c6c671f6e283a30028
smicle <bhhs1270026@gn.iwasaki.ac.jp>
2019-10-30 17:40:52 +0900

    Add Store and Todo Component

---
+++ components/TodoList.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>{{message}}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator'
+
+@Component
+export default class TodosList extends Vue {
+  message: string = 'Todo List 😎'
+}
+</script>
